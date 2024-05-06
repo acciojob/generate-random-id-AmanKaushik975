7f60a1ad7225afe9883d2ad3f67af99da9110ee2
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-05-06 18:57:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function makeid(l) {
   // write your code here
-	let res = '';
-	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	
-	for (let i = 0; i < l; i++) {
-	  res += characters.charAt(Math.floor(Math.random()*characters.length)) 
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < l; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-
-  return res;
+  return result;
 }
-// Do not change the code below.
-//const l = prompt("Enter a number.");
+
+//Do not change the code below.
+const l = prompt("Enter a number.");
 alert(makeid(l));
